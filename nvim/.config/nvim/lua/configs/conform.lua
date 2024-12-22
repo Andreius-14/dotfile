1b5fe432e08d6a11ccef6a68b2633ca01a610005
Andreius_14 <@outlook.com>
2024-12-22 13:52:59 -0500

    Edicion Nvim

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,19 +1,19 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    css = { "prettier" },
-    html = { "prettier" },
-    javascript = { "standardjs" },
-    python = { "black" },
-    bash = { "shfmt" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		css = { "prettier" },
+		html = { "prettier" },
+		javascript = { "standardjs" },
+		python = { "black" },
+		bash = { "shfmt" },
+	},
 
-  format_on_save = {
-    async = true,
-    timeout_ms = 1000,
-    lsp_fallback = true,
-    quiet = true,
-  },
+	format_on_save = {
+		async = true,
+		timeout_ms = 1000,
+		lsp_fallback = true,
+		quiet = true,
+	},
 }
 
 return options
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,51 +1,45 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  -- ====================================
-  -- MIS CONFIGURACIONES
-  -- ====================================
+	-- ====================================
+	-- MIS CONFIGURACIONES
+	-- ====================================
 
-  {
-    "williamboman/mason.nvim",
-    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-    opts = function()
-      return require "nvchad.configs.mason"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		event = "User FilePost",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    event = "User FilePost",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-    end,
-  },
+	-- =================================
+	-- RESALTADO DE COLORES
+	-- =================================
 
-  -- =================================
-  -- RESALTADO DE COLORES
-  -- =================================
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("configs._nvim-treesitter")
+		end,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = require "configs.nvim-treesitter",
-  },
-
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require "configs.indent-blankline"
-    end,
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("configs._indent-blankline")
+		end,
+	},
 }
